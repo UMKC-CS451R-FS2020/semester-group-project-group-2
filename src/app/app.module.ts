87968c614da5b2b99f6569cd83c05aef1bcb308e
Jimmy Blundell <42780250+JimmyBlundell@users.noreply.github.com>
2020-11-16 19:41:42 -0600

    Revert "Transaction component"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {transLogComponent} from './transLog/transLog.component';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClient} from '@angular/common/http';
-import { MakeTransComponent } from './make-trans/make-trans.component';
-import { NotifyrulesComponent } from './notifyrules/notifyrules.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { HomeComponent } from './home/home.component';
-import { TransactionComponent } from './transaction/transaction.component';
-import { LoginComponent } from './login/login.component'
-import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    transLogComponent,
-    MakeTransComponent,
-    NotifyrulesComponent,
-    NotificationsComponent,
-    HomeComponent,
-    TransactionComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent,
-    transLogComponent,
-    MakeTransComponent,
-    NotifyrulesComponent,
-    NotificationsComponent,
-    HomeComponent,
-    TransactionComponent,
-    LoginComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
