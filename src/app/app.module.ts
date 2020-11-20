e62fd4ac0679d81509438887b60333f79c2aab55
josiahconely <josiahconely@gmail.com>
2020-11-20 09:38:35 -0600

    Fixed removed transLog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {transLogComponent} from './transLog/transLog.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient} from '@angular/common/http';
 import { MakeTransComponent } from './make-trans/make-trans.component';
@@ -53,7 +53,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   ],
   providers: [],
   bootstrap: [AppComponent,
-    transLogComponent,
+    
     MakeTransComponent,
     NotifyrulesComponent,
     NotificationsComponent,
