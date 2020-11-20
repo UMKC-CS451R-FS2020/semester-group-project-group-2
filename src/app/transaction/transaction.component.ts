0cbb5466f785c5a37bd3f288f06151f95c86fb84
Jimmy Blundell <jblundell123@gmail.com>
2020-11-20 11:08:20 -0600

    transactions table added
    adding transactions table

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -6,10 +6,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transaction.component.css']
 })
 export class TransactionComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  title = 'dynamic-mat-table';
+  //fetch table cols
+  tableCols = ['Date', 'Type', 'Amount', 'Vendor', 'State', 'Balance'];
+  //fetch table contents
+  tableData = [
+    {
+      Date: '20-10-02',
+      Type: 'Deposit Made',
+      Amount: '$+200.00',
+      Vendor: '$200.00',
+      State: 'MO',
+      Balance: '1000'
+    },
+    {
+      Date: '20-10-02',
+      Type: 'Deposit Made',
+      Amount: '$+200.00',
+      Vendor: '$200.00',
+      State: 'MO',
+      Balance: '1000'
+    },
+    {
+      Date: '20-10-02',
+      Type: 'Deposit Made',
+      Amount: '$+200.00',
+      Vendor: '$200.00',
+      State: 'MO',
+      Balance: '1000'
+    },
+    {
+      Date: '20-10-02',
+      Type: 'Deposit Made',
+      Amount: '$+200.00',
+      Vendor: '$200.00',
+      State: 'MO',
+      Balance: '1000'
+    },
+    {
+      Date: '20-10-02',
+      Type: 'Deposit Made',
+      Amount: '$+200.00',
+      Vendor: '$200.00',
+      State: 'MO',
+      Balance: '1000'
+    },
+    {
+      Date: '20-10-02',
+      Type: 'Deposit Made',
+      Amount: '$+200.00',
+      Vendor: '$200.00',
+      State: 'MO',
+      Balance: '1000'
+    },
+    {
+      Date: '20-10-02',
+      Type: 'Deposit Made',
+      Amount: '$+200.00',
+      Vendor: '$200.00',
+      State: 'MO',
+      Balance: '1000'
+    },
+    {
+      Date: '20-10-02',
+      Type: 'Deposit Made',
+      Amount: '$+200.00',
+      Vendor: '$200.00',
+      State: 'MO',
+      Balance: '1000'
+    },
+    {
+      Date: '20-10-02',
+      Type: 'Deposit Made',
+      Amount: '$+200.00',
+      Vendor: '$200.00',
+      State: 'MO',
+      Balance: '1000'
+    },
+  ];
 }
