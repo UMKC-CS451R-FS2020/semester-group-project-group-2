bb595cef96a3c58475a5f420040b3b1ad43960b4
jacobalang <70710706+jacobalang@users.noreply.github.com>
2020-11-23 20:41:50 -0600

    hope
    dd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { NotificationsComponent } from './notifications/notifications.component';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
+import {TransactionComponent} from './transaction/transaction.component'
+const routes: Routes = [
+  { path: 'notifications', component: NotificationsComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '', component: HomeComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'transactions', component: TransactionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
