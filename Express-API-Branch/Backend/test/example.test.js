f9d78b7440acae6bf6619029bfaf5d4509bf8c98
josiahconely <josiahconely@gmail.com>
2020-12-10 20:10:11 -0600

    testing?

--- Express-API-Branch/Backend/test/example.test.js
+++ Express-API-Branch/Backend/test/example.test.js
@@ -1,57 +1,66 @@
-
 const assert = require('assert');
+const User = require('../Models/User');
+const Transaction = require('../Models/Transaction');
 
 //describe — a logical grouping of tests, “Simple Math Test”
-describe('Simple Math Test', () => {
-  //it — a single test,
-  it('should return 2', () => {
-    //assert — how you validate your test works or fails
-    assert.equal(1 + 1, 2);
-  });
-  it('should return 9', () => {
-    assert.equal(3 * 3, 9);
+describe('User', function() {
+  describe('#save()', function() {
+    it('should save without error', function(done) {
+      var user = new User({"username":'Luna', "password":"abc123"});
+      user.save().then(done()); 
+    });
   });
 });
 
 
 
-describe('hooks', function() {
-  before(function() {
-    // runs once before the first test in this block
-    var db = new Connection(),
-      tobi = new User('tobi'),
-      loki = new User('loki'),
-      jane = new User('jane');
-  });
-
-  after(function() {
-    // runs once after the last test in this block
-  });
-
-  beforeEach(function(done) {
-    db.clear(function(err) {
-      if (err) return done(err);
-      db.save([tobi, loki, jane], done);
-    });
-    // runs before each test in this block
-  });
-
-  afterEach(function() {
-    // runs after each test in this block
-  });
-
-  describe('#find()', function() {
-    it('respond with matching records', function(done) {
-      db.find({
-        type: 'User'
-      }, function(err, res) {
-        if (err) return done(err);
-        res.should.have.length(3);
-        done();
-      });
-    });
-  });
-  // test cases
-});
-
-// to run, npm run test in /Backend dir
+//
+// describe('hooks', function() {
+//   before(function() {
+//     // runs once before the first test in this block
+//     const user = new User({
+//       username: "Billy",
+//       password: "1234abcd",
+//       account: ""
+//     });
+//     user.save();
+//   });
+//
+//   after(function() {
+//     User.findOne({
+//       username: "Billy"
+//     }).then(data => {
+//       assert.equal("Billy", data.username);
+//     });
+//
+//
+//     // runs once after the last test in this block
+//   });
+//
+//   beforeEach(function(done) {
+//     db.clear(function(err) {
+//       if (err) return done(err);
+//       db.save([tobi, loki, jane], done);
+//     });
+//     // runs before each test in this block
+//   });
+//
+//   afterEach(function() {
+//     // runs after each test in this block
+//   });
+//
+//   describe('#find()', function() {
+//     it('respond with matching records', function(done) {
+//       db.find({
+//         type: 'User'
+//       }, function(err, res) {
+//         if (err) return done(err);
+//         res.should.have.length(3);
+//         done();
+//       });
+//     });
+//   });
+//   // test cases
+// });
+//
+// // to run, npm run test in /Backend dir
